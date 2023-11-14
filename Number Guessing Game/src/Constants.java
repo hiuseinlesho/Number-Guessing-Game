847ff59380ea1dbae9c38f1bc2846cc48e3f6f56
Hiusein Lesho <133807047+hiuseinlesho@users.noreply.github.com>
2023-11-14 22:52:11 +0200

    Add class Constants

---
+++ Number Guessing Game/src/Constants.java
@@ -0,0 +1,10 @@
+public class Constants {
+
+    public static final String MESSAGE = "Enter your guess: ";
+    public static final String DIFFICULTIES = "1. Easy - between 1 - 20\n" +
+            "2. Medium - between 1 - 50\n" +
+            "3. Hard - between 1 - 100\n";
+    public static final String TITLE = "Number Guessing Game\n" +
+             "Guess the number between 1 and X\n";
+
+}
--- Number Guessing Game/src/Main.java
+++ Number Guessing Game/src/Main.java
@@ -3,21 +3,15 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static final String MESSAGE = "Enter your guess: ";
-    public static final String DIFFICULTIES = "1. Easy - between 1 - 20\n" +
-            "2. Medium - between 1 - 50\n" +
-            "3. Hard - between 1 - 100\n";
-
     public static void main(String[] args) {
-        System.out.println("Number Guessing Game\n" +
-                "Guess the number between 1 and X\n");
+        System.out.println(Constants.TITLE);
 
         System.out.println("Choose difficulty level:");
-        System.out.println(DIFFICULTIES);
+        System.out.println(Constants.DIFFICULTIES);
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter your choice: ");
+        System.out.print(Constants.MESSAGE);
         int n = Integer.parseInt(scanner.nextLine());
 
         Random random = new Random();
